eaf2655722bea73c117d3d19bb932563ebb0439f
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-18 09:58:37 -0600

    bomb setup complete

--- app.js
+++ app.js
@@ -1,69 +1,54 @@
-// import functions and grab DOM elements
-const form = document.getElementById('ingredient-list');
-const remove = document.getElementById('remove');
-const listedingredients = document.getElementById('listed');
-const save = document.getElementById('meal-button');
-const meal = document.getElementById('recipe-name');
-const history = document.getElementById('history');
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const grid = document.querySelector('.grid');
+    let width = 15;
+    // let height = 20;
+    let bombAmount = 25;
 
-import { renderIngredient } from './utils.js';
-import { renderSoloMeal } from './utils.js';
+    let squares = [];
 
-// let state
-let mealsArray = [];
-let totalstuff = [];
+    function createboard() {
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const totalList = new FormData(form);
+        // shuffled game with bombs
+        const bombsArray = Array(bombAmount).fill('bomb');
+        const emptyArray = Array(width * width - bombAmount).fill('vaild');
+        const gameArray = emptyArray.concat(bombsArray);
+        const shuffleArray = gameArray.sort(() => Math.random() - 0.5);
 
-    let ingredients = {
-        name: totalList.get('food'),
-        quantity: totalList.get('amount'),
-        measurement: totalList.get('measure'),
-    };
-    totalstuff.push(ingredients);
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement('div');
+            square.setAttribute('id', i);
+            square.classList.add(shuffleArray[i]);
+            grid.appendChild(square);
+            squares.push(square);
+        }
+        for (let i = 0; i < squares.length; i++) {
+            let total = 0;
+            const leftEdge = (i % width === 0);
+            const rightEdge = (i % width === width - 1);
+
+            if (squares[i].classList.contains('valid')) {
+                if (i > 0 && !leftEdge && squares[i - 1].classList.contains('bomb')) total ++;
+                if (i > 14 && !rightEdge && squares[i + 1 - width].classList.contains('bomb')) total ++;
+            }
+        }
 
 
-    displayingredients();
-    //form.reset();
-});
 
-remove.addEventListener('click', () => {
-    totalstuff.pop();
-    displayingredients();
-});
 
-function displayingredients() {
-    listedingredients.textContent = '';
-    for (let list of totalstuff) {
-        const li = renderIngredient(list);
-        listedingredients.appendChild(li);
+
+
+
+
+
+
     }
-}
-//function resetIngredients() {
-  //  totalstuff = [];
-    //listedingredients.textcontent = '';
-//}
+    createboard();
 
-save.addEventListener('click', () => {
-    const mealName = meal.value;
-    const ingredcount = totalstuff.length;
-    mealsArray.push({ mealName, ingredcount });
-    renderMeals();
-    console.log(renderMeals);
-    console.log(mealsArray);
+
+
+
+
+
+
 });
-
-function renderMeals() {
-    history.textContent = '';
-    for (let meal of mealsArray) {
-        const li = renderSoloMeal(meal);
-        history.append(li);
-    }
-}
-//name="recipe" id="recipe-name"/>
